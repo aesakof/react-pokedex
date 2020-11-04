ccd5b6db70f8f2b3a3ce3c3b5e984f512649c099
aesakof <aesakof@gmail.com>
2020-11-03 21:05:28 -0500

    Added Select menu for type filtering

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,7 +1,29 @@
-import React, { useEffect, useState, useContext } from "react"
+import React, { useContext } from "react"
+import Select from 'react-select';
+
 import PokemonPreview from "./PokemonPreview"
 import {Context} from "./../Context"
 
+const options = [
+    { value: 'normal', label: 'Normal' },
+    { value: 'fire', label: 'Fire' },
+    { value: 'water', label: 'Water' },
+    { value: 'grass', label: 'Grass' },
+    { value: 'electric', label: 'Electric' },
+    { value: 'ice', label: 'Ice' },
+    { value: 'fighting', label: 'Fighting' },
+    { value: 'poison', label: 'Poison' },
+    { value: 'ground', label: 'Ground' },
+    { value: 'flying', label: 'Flying' },
+    { value: 'psychic', label: 'Psychic' },
+    { value: 'bug', label: 'Bug' },
+    { value: 'rock', label: 'Rock' },
+    { value: 'ghost', label: 'Ghost' },
+    { value: 'dark', label: 'Dark' },
+    { value: 'dragon', label: 'Dragon' },
+    { value: 'steel', label: 'Steel' },
+    { value: 'fairy', label: 'Fairy' },
+  ];
 
 function PokemonList(props) {
     const { pokemon, setPage, prevUrl, nextUrl } = useContext(Context)
@@ -13,11 +35,24 @@ function PokemonList(props) {
         return (<PokemonPreview key={poke.name} pokemon={poke} />)
     })
 
+    function handleChange(selectedOption) {
+        console.log(`Option selected:`, selectedOption);
+    };
+
     return (
         <div>
+            <div className="select-container">
+                <label id='filter-label'>Filter By Type:</label>
+                <Select
+                    className="type-select"
+                    options={options}
+                />
+            </div>
+
             <div className="pokemon-list">
                 {monz}
             </div>
+
             <div id="pagination">
                 {prevUrl && <button className="page-buttons" onClick={() => setPage(beginningUrl)}>First</button>}
                 {prevUrl && <button className="page-buttons" onClick={() => setPage(prevUrl)}>Previous</button>}
