3fc58726f474055f74a2368e80ac7e8d5196e348
aesakof <aesakof@gmail.com>
2025-05-31 22:26:18 -0400

    Switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {BrowserRouter as Router} from "react-router-dom"
+import {HashRouter as Router} from "react-router-dom"
 
 import App from "./App"
 import { ContextProvider } from "./Context"
 
 ReactDOM.render(
     <ContextProvider>
-        <Router basename="/pokedex">
+        <Router>
             <App />
         </Router>
     </ContextProvider>,
